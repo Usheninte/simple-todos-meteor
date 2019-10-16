e4d7346903b7d9c866238240cb235a368c15a746
Usheninte <jrandangana@gmail.com>
2019-10-16 15:20:30 +0000

    accounts ui wrapper

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { withTracker } from 'meteor/react-meteor-data';
 
 import { Tasks } from '../api/tasks.js';
+import AccountsUIWrapper from './AccountsUIWrapper.js';
 
 import Task from './Task.js';
 
@@ -60,6 +61,8 @@ class App extends Component {
             />
             Hide Completed Tasks
           </label>
+
+          <AccountsUIWrapper />
 
           <form className="new-task" onSubmit={this.handleSubmit.bind(this)}>
             <input
