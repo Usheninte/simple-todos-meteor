0c711e9b1e48039f4d58a7161850e9002a42e0b7
Usheninte <jrandangana@gmail.com>
2019-10-16 14:33:56 +0000

    commit

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -18,14 +18,6 @@ class App extends Component {
   //   ];
   // }
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      hideCompleted: false,
-    };
-  }
-
   handleSubmit(event) {
     event.preventDefault();
 
@@ -43,35 +35,16 @@ class App extends Component {
     ReactDOM.findDOMNode(this.refs.textInput).value = '';
   }
 
-  toggleHideCompleted() {
-    this.setState({
-      hideCompleted: !this.state.hideCompleted,
-    });
-  }
-
   renderTasks() {
-    let filteredTasks = this.props.tasks;
-    if (this.state.hideCompleted) {
-      filteredTasks = filteredTasks.filter(task => !task.checked);
-    }
-    return filteredTasks.map(task => <Task key={task._id} task={task} />);
+    return this.props.tasks.map(task => <Task key={task._id} task={task} />);
+    // return this.getTasks().map(task => <Task key={task._id} task={task} />);
   }
 
   render() {
     return (
       <div className="container">
         <header>
-          <h1>Todo List ({this.props.incompleteCount})</h1>
-
-          <label className="hide-completed">
-            <input
-              type="checkbox"
-              readOnly
-              checked={this.state.hideCompleted}
-              onClick={this.toggleHideCompleted.bind(this)}
-            />
-            Hide Completed Tasks
-          </label>
+          <h1>Todo List</h1>
 
           <AccountsUIWrapper />
 
@@ -97,7 +70,10 @@ class App extends Component {
 export default withTracker(() => {
   return {
     tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
+<<<<<<< HEAD
     incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),
     currentUser: Meteor.user(),
+=======
+>>>>>>> parent of 396586f... temporary ui state
   };
 })(App);
