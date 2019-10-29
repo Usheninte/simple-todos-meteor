d07652f7149a672556eba4490f901a7fa8292811
Ninte <ninte.dev@gmail.com>
2019-10-29 15:37:51 +0000

    trimmed file

--- imports/api/tasks.tests.js
+++ imports/api/tasks.tests.js
@@ -39,7 +39,6 @@ if (Meteor.isServer) {
 
       it('Can insert task', () => {
         const addTask = Meteor.server.method_handlers['tasks.insert'];
-
         const invocation = { userId };
         let text = 'test text';
 
@@ -49,23 +48,16 @@ if (Meteor.isServer) {
       });
 
       it('Can delete own task', () => {
-        // Find the internal implementation of the task method so we can
-        // test it in isolation
         const deleteTask = Meteor.server.method_handlers['tasks.remove'];
-
-        // Set up a fake method invocation that looks like what the method expects
         const invocation = { userId };
 
-        // Run the method with `this` set to the fake invocation
         deleteTask.apply(invocation, [taskId]);
 
-        // Verify that the method does what we expected
         assert.equal(Tasks.find().count(), 0);
       });
 
       it('Can not insert task if not logged in', () => {
         const addTask = Meteor.server.method_handlers['tasks.insert'];
-
         const invocation = {};
         let text = 'test text';
 
@@ -81,7 +73,6 @@ if (Meteor.isServer) {
       });
 
       it("Can not delete someone else's task", () => {
-        // Set task to private
         let setToPrivate = true;
         Tasks.update(taskId, { $set: { private: setToPrivate } });
 
@@ -94,7 +85,6 @@ if (Meteor.isServer) {
       });
 
       it('Can set own task checked', () => {
-        // set task as checked
         let setChecked = true;
         Tasks.update(taskId, { $set: { checked: setChecked } });
 
