7757a3d534518521786f5c84765ef0e09524f973
Ninte <ninte.dev@gmail.com>
2019-10-29 13:16:42 +0000

    added dummy user credentials to test

--- imports/api/tasks.tests.js
+++ imports/api/tasks.tests.js
@@ -2,16 +2,31 @@
 
 import { Meteor } from 'meteor/meteor';
 import { Random } from 'meteor/random';
-
 import { assert } from 'chai';
+import { Accounts } from 'meteor/accounts-base';
 
 import { Tasks } from './tasks.js';
 
 if (Meteor.isServer) {
   describe('Tasks', () => {
     describe('methods', () => {
-      const userId = Random.id();
+      let userId = Random.id();
       let taskId;
+      const username = 'userzero';
+
+      before(() => {
+        // Create user if not already created
+        let user = Meteor.users.findOne({ username: username });
+        if (!user) {
+          userId = Accounts.createUser({
+            username: username,
+            email: username,
+            password: '1234567890',
+          });
+        } else {
+          userId = user._id;
+        }
+      });
 
       beforeEach(() => {
         Tasks.remove({});
@@ -23,29 +38,21 @@ if (Meteor.isServer) {
         });
       });
 
-      // it('can insert task', () => {
-      //   const addTask = Meteor.server.method_handlers['tasks.insert'];
+      it('can insert task', () => {
+        const addTask = Meteor.server.method_handlers['tasks.insert'];
 
-      //   const invocation = { userId };
-      //   let text = 'test text';
-
-      //   assert.throws(
-      //     function() {
-      //       addTask.apply(invocation, [text]);
-      //     },
-      //     Meteor.Error,
-      //     '[not-authorized]',
-      //   );
-
-      //   assert.equal(Tasks.find().count(), 2);
-      // });
-
-      it('cannot delete task if not logged in', () => {
         const invocation = { userId };
+        let text = 'test text';
 
-        if (invocation === null) {
-          Meteor.Error('Please login to add tasks.');
-        }
+        assert.throws(
+          function() {
+            addTask.apply(invocation, [text]);
+          },
+          Meteor.Error,
+          '[not-authorized]',
+        );
+
+        assert.equal(Tasks.find().count(), 2);
       });
 
       it('can delete owned task', () => {
@@ -62,11 +69,6 @@ if (Meteor.isServer) {
         // Verify that the method does what we expected
         assert.equal(Tasks.find().count(), 0);
       });
-
-      // it("can not delete someone else's task", () => {
-      //   const invocation = { userId };
-
-      // });
     });
   });
 }
